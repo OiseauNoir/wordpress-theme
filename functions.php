56c2d2660e50d90766fc336e3ca04223acbcf2a5
Riley Dallas <rldaggie@gmail.com>
2016-10-05 11:34:41 -0500

    Added font awesome css

--- functions.php
+++ functions.php
@@ -7,6 +7,8 @@ function wednesday_setup() {
 
   // Stylesheets
   wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/vendor/bootstrap/css/bootstrap.min.css',false,'3.3.7','all');
+  wp_enqueue_style( 'font-awesome', get_template_directory_uri() . '/vendor/font-awesome/css/font-awesome.min.css',false,'4.6.3','all');
+
   wp_enqueue_style( 'clean-blog', get_template_directory_uri() . '/css/clean-blog.min.css',array('bootstrap'),'3.3.7','all');
 }
 
