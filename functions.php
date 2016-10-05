ffaaa7eb7656ab6dfb9b977bf3de2b6af55d3090
Riley Dallas <rldaggie@gmail.com>
2016-10-05 11:22:41 -0500

    Add support for post thumbnails

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 if ( ! function_exists( 'wednesday_setup' ) ) :
 
 function wednesday_setup() {
-  add_theme_support('title-tag')
+  add_theme_support('title-tag');
+  add_theme_support( 'post-thumbnails' );
 }
 
 endif;
